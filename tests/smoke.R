071deb310afab80e5ddf7f68e7bfb62e1bc0551b
CryptiqCO <167649856+CryptiqCO@users.noreply.github.com>
2026-03-17 11:06:00 -0600

    test-smoke-script

---
+++ tests/smoke.R
@@ -0,0 +1,4 @@
+﻿e <- new.env()
+sys.source(file.path("R", "normalize.R"), envir = e)
+normalize_agent_line <- get("normalize_agent_line", envir = e)
+stopifnot(normalize_agent_line("  hi   ") == "hi")
