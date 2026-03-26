05435f14e56f723b4968c630363ca429e90a9cc0
CryptiqCO <167649856+CryptiqCO@users.noreply.github.com>
2026-03-26 09:47:00 -0600

    docs-inline-note-normalize

--- R/normalize.R
+++ R/normalize.R
@@ -3,3 +3,5 @@ normalize_agent_line <- function(x) {
   x <- gsub("^\\s+|\\s+$", "", x, perl = TRUE)
   gsub("\\s{2,}", " ", x, perl = TRUE)
 }
+
+# whitespace normalization only - callers decide encoding
