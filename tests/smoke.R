88abd2b2154667fc966c468e173c05333a5c995e
CryptiqCO <167649856+CryptiqCO@users.noreply.github.com>
2026-03-29 09:05:00 -0600

    test-empty-string

--- tests/smoke.R
+++ tests/smoke.R
@@ -2,3 +2,5 @@
 sys.source(file.path("R", "normalize.R"), envir = e)
 normalize_agent_line <- get("normalize_agent_line", envir = e)
 stopifnot(normalize_agent_line("  hi   ") == "hi")
+
+stopifnot(normalize_agent_line('') == '')
